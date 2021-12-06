d6ccb41e362ea71415530ce25649edd55de7da40
GeorgeL <971462342@qq.com>
2021-12-06 19:12:22 +0800

    update

--- StudentManagement/Startup.cs
+++ StudentManagement/Startup.cs
@@ -1,6 +1,7 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,6 +13,12 @@ namespace StudentManagement
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -32,8 +39,10 @@ namespace StudentManagement
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    var processName = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
-                    await context.Response.WriteAsync(processName + "1");
+                    // 进程名
+                    // var processName = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
+                    var configVal = _configuration["Mykey"];
+                    await context.Response.WriteAsync(configVal);
                 });
             });
         }
